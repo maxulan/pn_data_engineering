5247191b24cb3ed747610e77d1f7b74d67f12aed
maxim <maxulan@mail.ru>
2019-07-27 19:23:49 +0300

    added domain objects

---
+++ src/main/scala/net/pubnative/domain/Click.scala
@@ -0,0 +1,6 @@
+package net.pubnative.domain
+
+case class Click(
+                  impression_id: String,
+                  revenue: Double
+                )
---
+++ src/main/scala/net/pubnative/domain/Impression.scala
@@ -0,0 +1,8 @@
+package net.pubnative.domain
+
+case class Impression(
+                       id: String,
+                       app_id: Int,
+                       advertiser_id: Int,
+                       country_code: Option[String]
+                     )
