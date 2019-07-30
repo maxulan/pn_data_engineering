e784e123ef82ab4f71b059649b55224eea9c093e
maxim <maxulan@mail.ru>
2019-07-30 05:50:39 +0300

    added unit-test for MasterActor

--- src/test/scala/net/pubnative/MainSpec.scala
+++ src/test/scala/net/pubnative/MainSpec.scala
@@ -1,19 +1,20 @@
-//#full-example
 package net.pubnative
 
-import org.scalatest.{ BeforeAndAfterAll, WordSpecLike, Matchers }
+import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpecLike}
 import akka.actor.ActorSystem
-import akka.testkit.{ TestKit, TestProbe }
+import akka.testkit.{TestKit, TestProbe}
+import net.pubnative.actors.MasterActor
+import net.pubnative.commands.TotalProcessingResult
+
 import scala.concurrent.duration._
 import scala.language.postfixOps
 
 //#test-classes
 class MainSpec(_system: ActorSystem)
   extends TestKit(_system)
-  with Matchers
-  with WordSpecLike
-  with BeforeAndAfterAll {
-  //#test-classes
+    with Matchers
+    with WordSpecLike
+    with BeforeAndAfterAll {
 
   def this() = this(ActorSystem("PubNativeSpec"))
 
@@ -21,20 +22,17 @@ class MainSpec(_system: ActorSystem)
     shutdown(system)
   }
 
-  //#first-test
-  //#specification-example
-  "A Greeter Actor" should {
-    "pass on a greeting message when instructed to" in {
-      //#specification-example
-      val testProbe = TestProbe()
-      val helloGreetingMessage = "hello"
-//      val helloGreeter = system.actorOf(Greeter.props(helloGreetingMessage, testProbe.ref))
-//      val greetPerson = "Akka"
-//      helloGreeter ! WhoToGreet(greetPerson)
-//      helloGreeter ! Greet
-//      testProbe.expectMsg(500 millis, Greeting(helloGreetingMessage + ", " + greetPerson))
+  "A Master Actor" should {
+    "manage computation pipeline of App/Country report and recommendation" in {
+
+      val files: Array[String] = Array(
+        getClass.getResource("/clicks.json").getFile,
+        getClass.getResource("/impressions.json").getFile
+      )
+      val master = system.actorOf(MasterActor.props, "master")
+
+      master ! files
+      expectMsgType[TotalProcessingResult](20 seconds)
     }
   }
-  //#first-test
 }
-//#full-example
